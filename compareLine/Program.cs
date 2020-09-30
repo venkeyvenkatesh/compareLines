1da3914b869e7d8985068fefe47dee5855c6239f
venkey <venkeyvenkatesh885@gmail.com>
2020-09-30 11:28:01 +0530

    first commit

---
+++ compareLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace compareLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation program!!");
+        }
+    }
+}
