f697c9055201873a904960cf6145cb8b69e67d90
venkey <venkeyvenkatesh885@gmail.com>
2020-09-30 11:59:26 +0530

    [venkey] Refactor. calculated length of line

---
+++ compareLine/LineClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace compareLine
+{
+    class LineClass
+    {
+        //variables
+        int xCoOrdinate1, yCoOrdinate1;
+        int xCoOrdinate2, yCoOrdinate2;
+
+        public LineClass(int xCoOrdinate1, int yCoOrdinate1, int XCoOrdinate2, int yCoOrdinate2)
+        {
+            this.xCoOrdinate1 = xCoOrdinate1;
+            this.xCoOrdinate2 = XCoOrdinate2;
+            this.yCoOrdinate1 = yCoOrdinate1;
+            this.yCoOrdinate2 = yCoOrdinate2;
+        }
+
+        public double find_length()
+        {
+            double length = Math.Sqrt(Math.Pow(xCoOrdinate2 - xCoOrdinate1, 2) + Math.Pow(yCoOrdinate2 - yCoOrdinate1, 2) * 1.0);
+            return Math.Round(length, 2);
+        }
+    }
+}
--- compareLine/Program.cs
+++ compareLine/Program.cs
@@ -7,6 +7,11 @@ namespace compareLine
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparision Computation program!!");
+
+
+          LineClass line1 = new LineClass(11, 25, 10, 6);
+           Console.WriteLine("The length of the line is : "+line1.find_length());
+
         }
     }
 }
